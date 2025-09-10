1c659c7bca8c68f518a537bb3ab553e485a532f3
MikaelAlcantara <gundeloratao@gmail.com>
2025-09-10 17:53:59 -0300

    attt

--- ACESSO RESTRITO/firebase-config.js
+++ ACESSO RESTRITO/firebase-config.js
@@ -1,7 +1,11 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBV3ny1x2Y_JXylVspt797G5p56zCJ-Gco",
   authDomain: "lumenni-estetica-autentication.firebaseapp.com",
@@ -13,7 +17,6 @@ const firebaseConfig = {
   measurementId: "G-B433W9ZE86"
 };
 
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
-
-export default firebase;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
